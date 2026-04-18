7bd61c8d60c4d3db98400228fd698963e5dbf5a7
Eren Kaan Oral <110434815+erenko147@users.noreply.github.com>
2026-04-18 19:56:35 +0300

    Delete unity directory

--- unity/Scripts/VehicleAgent.cs
+++
@@ -1,5 +0,0 @@
-// Bu dosya artik kullanilmiyor.
-// Arac renderlamasi VehiclePool.cs icinde GPU Instancing ile yapiliyor.
-// Silebilirsin ya da oldugu gibi birakabilirsin - derlemeyi etkilemez.
-using UnityEngine;
-public class VehicleAgent : MonoBehaviour { }
